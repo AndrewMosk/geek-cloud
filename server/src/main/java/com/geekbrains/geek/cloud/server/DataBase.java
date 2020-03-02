acceaa62ac019dd52854a6bc868416e0b7d76549
Andrew Moskalets <MoskaletsAndrey@yandex.ru>
2020-03-02 16:14:58 +0300

    изменил БД с sqllite на postgres

--- server/src/main/java/com/geekbrains/geek/cloud/server/DataBase.java
+++ server/src/main/java/com/geekbrains/geek/cloud/server/DataBase.java
@@ -6,10 +6,14 @@ class DataBase {
     private static Connection connection;
     private static Statement stmt;
 
+    static final String DB_URL = "jdbc:postgresql://127.0.0.1:5432/users";
+    static final String USER = "postgres";
+    static final String PASS = "qwerty";
+
     static void connect(){
         try {
-            Class.forName("org.sqlite.JDBC");
-            connection = DriverManager.getConnection("jdbc:sqlite:server/users.db");
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection(DB_URL, USER, PASS);
             stmt = connection.createStatement();
         } catch (ClassNotFoundException|SQLException e) {
             e.printStackTrace();
@@ -31,6 +35,7 @@ class DataBase {
 
     static void disconnect(){
         try {
+            stmt.close();
             connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -38,7 +43,7 @@ class DataBase {
     }
 
     private static boolean tryToAuth(String login, int hash) throws SQLException {
-        ResultSet rs = stmt.executeQuery(String.format("SELECT * FROM Users WHERE login = '%s' AND passwordHash = '%s'", login, hash));
+        ResultSet rs = stmt.executeQuery(String.format("SELECT * FROM public.\"Users\" WHERE login = '%s' AND \"passwordHash\" = '%s'", login, hash));
 
         return rs.next();
     }
@@ -61,12 +66,12 @@ class DataBase {
     }
 
     private static boolean loginIsFree(String login) throws SQLException {
-        ResultSet rs = stmt.executeQuery(String.format("SELECT * FROM Users WHERE login = '%s'", login));
+        ResultSet rs = stmt.executeQuery(String.format("SELECT * FROM public.\"Users\" WHERE login = '%s'", login));
 
         return !rs.next();
     }
 
     private static void tryToReg(String login, int hash) throws SQLException {
-        stmt.execute(String.format("INSERT INTO Users VALUES ('%s', '%s')", login, hash));
+        stmt.execute(String.format("INSERT INTO public.\"Users\" VALUES ('%s', '%s')", login, hash));
     }
 }
